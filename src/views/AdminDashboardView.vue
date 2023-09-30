3ae05bf554ac620846f1f13d50bb2d4e30d83efc
chandya <chandya.c99@gmail.com>
2023-10-01 02:18:30 +0800

    typo

--- src/views/AdminDashboardView.vue
+++ src/views/AdminDashboardView.vue
@@ -25,7 +25,7 @@ onMounted(async () => {
 
 <template>
     <h1 class="text-3xl mb-8" v-if="role=='MANAGER'">{{ $t('admindashboard') }}</h1>
-    <h1 class="text-3xl mb-8 bg-red-100 border border-red-400 text-red-700 px-4 py-3 relative" v-else>{{ $t('noaccess') }}e</h1>
+    <h1 class="text-3xl mb-8 bg-red-100 border border-red-400 text-red-700 px-4 py-3 relative" v-else>{{ $t('noaccess') }}</h1>
     <p>{{ data }}</p>
     
 </template>
